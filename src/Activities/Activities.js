e541692c5474b70ef73979fc89747206ed632af6
Nick Taylor <nickstaylor@gmail.com>
2020-08-13 19:55:02 -0600

    Initial Stlying for Header and also added Favorites component.

---
+++ src/Activities/Activities.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Activities.css'
+
+const Activities = () => {
+  return (
+    <div className="activities-container">
+      <h2>Past Activities</h2>
+      I'm the Activities
+    </div>
+  )
+}
+
+export default Activities;
---
+++ src/Timer/Timer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Timer.css'
+
+const Timer = () => {
+  return (
+    <div>
+   I'm the Timer!
+    </div>
+  )
+}
+
+export default Timer;

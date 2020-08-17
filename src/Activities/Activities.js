31789e6ef95cdda25e1dcfb92c2f6166f027a48b
Nick Taylor <nickstaylor@gmail.com>
2020-08-16 21:21:31 -0600

    Add map function to activities component to display activities.  Styling for component also added

--- src/Activities/Activities.js
+++ src/Activities/Activities.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import './Activities.css'
+import ActivityCard from '../ActivityCard/ActivityCard'
+import heartFilled from '../images/heart-filled.svg'
+import exercise from '../images/exercise.svg'
+import meditate from '../images/meditate.svg'
+import study from '../images/study.svg'
+import exerciseActive from '../images/exercise-active.svg'
+import meditateActive from '../images/meditate-active.svg'
+import studyActive from '../images/study-active.svg'
 
-const Activities = () => {
+
+const Activities = ({ activities }) => {
+
+  let displayedActivities = activities.map((activity, i) => {
+    return (
+      <ActivityCard
+        key={i}
+        activity={activity}
+      />
+    )
+  })
+  
   return (
     <div className="activities-container">
-      <h2>Past Activities</h2>
-      I'm the Activities
+      <section className="activity-section-header">
+        <h2>Past Activities</h2>
+    
+      </section>
+      <section className="activities-inner-container">
+        {displayedActivities}
+      </section>
     </div>
   )
 }

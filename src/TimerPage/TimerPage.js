f84b70c17f646e6ae7514aa59bf9105214844c31
Nick Taylor <nickstaylor@gmail.com>
2020-09-09 08:47:34 -0600

    On Timer Page, add UseEffect hook that will reset the minutes/seconds/pause if an activity is replayed

--- src/TimerPage/TimerPage.js
+++ src/TimerPage/TimerPage.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react'
 import './TimerPage.css'
 import replay from '../images/replay.svg'
 
-const TimerPage = ({activity, logActivity, setCurrentActivity}) => {
-  console.log('currentActivity', activity);
+const TimerPage = ({ activity, logActivity, setCurrentActivity }) => {
+  // console.log('currentActivity', activity);
   const [timerCompleted, setTimerCompleted] = useState(false)
   const [timerActive, setTimerActive] = useState(false)
   const [activityLogged, setActivityLogged] = useState(false)
@@ -11,19 +11,26 @@ const TimerPage = ({activity, logActivity, setCurrentActivity}) => {
   const [seconds, setSeconds] = useState(activity.sec)
 
   useEffect(() => {
+    setMinutes(activity.min)
+    setSeconds(activity.sec)
+    setTimerActive(false)
+
+  }, [activity])
+
+  useEffect(() => {
     let timer = null;
     if (timerActive) {
-    timer = setInterval(() => {
+      timer = setInterval(() => {
         setSeconds(seconds => seconds - 1);
       }, 1000);
-      if (minutes && seconds < 0){
-        setMinutes(minutes => minutes -1)
+      if (minutes && seconds < 0) {
+        setMinutes(minutes => minutes - 1)
         setSeconds(59)
       }
-      if (minutes === 0 && seconds === 0){
+      if (minutes === 0 && seconds === 0) {
         clearInterval(timer)
-        setTimeout(()=> setTimerCompleted(true), 1000)
-        setTimeout(()=> setTimerActive(false), 1000)
+        setTimeout(() => setTimerCompleted(true), 1000)
+        setTimeout(() => setTimerActive(false), 1000)
       }
     } else if (!timerActive) {
       clearInterval(timer);
@@ -51,30 +58,30 @@ const TimerPage = ({activity, logActivity, setCurrentActivity}) => {
     <div className="form-timer-container">
       <h2>Current Activity : <span className={`${activity.type}-header`}>{activity.type}</span></h2>
       <section className="timer-inner-container">
-      {!activityLogged ?
-        <>
-        <section className="timer-column-box">
-          {timerCompleted && <img onClick={reloadActivity} className="replay-img" src={replay} alt="replay" />}
-          <div className="top-timer-column-box">
-            <p>{activity.description}</p>
-            <p className="timer-display">{minutes}:{seconds < 10 ? '0' + seconds : seconds}</p>
-          </div>
-          <button className={`timer-circle-btn ${activity.type}-outline`} onClick={activateTimer}>
-          {timerCompleted ? 'congrats!' : (timerActive ? 'pause' : 'start')}
+        {!activityLogged ?
+          <>
+            <section className="timer-column-box">
+              {timerCompleted && <img onClick={reloadActivity} className="replay-img" src={replay} alt="replay" />}
+              <div className="top-timer-column-box">
+                <p>{activity.description}</p>
+                <p className="timer-display">{minutes}:{seconds < 10 ? '0' + seconds : seconds}</p>
+              </div>
+              <button className={`timer-circle-btn ${activity.type}-outline`} onClick={activateTimer}>
+                {timerCompleted ? 'congrats!' : (timerActive ? 'pause' : 'start')}
+              </button>
+            </section>
+            {timerCompleted &&
+              <button className="log-activity-btn" onClick={completeActivity}>
+                Log Activity
           </button>
-        </section>
-        {timerCompleted &&
-          <button className="log-activity-btn" onClick={completeActivity}>
-          Log Activity
-          </button>
-        }
-        </> :
+            }
+          </> :
 
-        <button className="new-activity-btn" onClick={()=> setCurrentActivity('')}>
-        Create a new activity
+          <button className="new-activity-btn" onClick={() => setCurrentActivity('')}>
+            Create a new activity
         </button>
 
-      }
+        }
       </section>
 
     </div>
